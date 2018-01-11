6df5712c132a77f31bd3d92f2091034d8d3ed707
rkp8000 <rpang.contact@gmail.com>
2018-01-10 18:35:54 -0800

    Register sentences under term admin form.

--- lexicon/admin.py
+++ lexicon/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 from .models import Term, Sentence
 
 
-admin.site.register(Term)
-admin.site.register(Sentence)
+class SentenceInline(admin.StackedInline):
+    model = Sentence
+    extra = 3
+
+
+class TermAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['term_text', 'priority']}),
+    ]
+    inlines = [SentenceInline]
+
+
+admin.site.register(Term, TermAdmin)
