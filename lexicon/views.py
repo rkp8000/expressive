6de3521e179af4faede7ef2b41158d1490613111
Rich Pang <rpang.contact@gmail.com>
2018-01-30 21:42:22 -0800

    Make example sentences show automatically when new sentence is added.

--- lexicon/views.py
+++ lexicon/views.py
@@ -19,6 +19,18 @@ class TermDetail(generic.DetailView):
     model = Term
     template_name = 'lexicon/term_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(TermDetail, self).get_context_data(**kwargs)
+        kwargs = self.kwargs
+
+        if 'show_example_sentences' in kwargs \
+                and kwargs['show_example_sentences'] == 'show_example_sentences':
+            context['show_example_sentences'] = True
+        else:
+            context['show_example_sentences'] = False
+
+        return context
+
 
 def random_term(request):
     term = random.choice(Term.objects.all())
@@ -27,9 +39,13 @@ def random_term(request):
 
 def add_sentence(request, term_id):
     term = get_object_or_404(Term, pk=term_id)
+    kwargs = {'pk': term_id}
     try:
         term.sentence_set.create(sentence_text=request.POST['sentence_text'])
+        kwargs['show_example_sentences'] = 'show_example_sentences'
     except:
         pass
+        kwargs['show_example_sentences'] = 'error'
 
-    return HttpResponseRedirect(reverse('lexicon:term_detail', args=(term.id,)))
+    return HttpResponseRedirect(
+        reverse('lexicon:term_detail_add_sentence', kwargs=kwargs))
